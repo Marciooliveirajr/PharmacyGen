c6bb2f07f241a7c17028a589e6eb47e3e2566d2d
unknown <marcio.oliveira.jr30@gmail.com>
2023-05-24 18:30:42 -0300

    Projeto desenvolvido utilizando Spring

---
+++ pharmacyGen/src/main/java/com/generation/farmaciagen/model/Categoria.java
@@ -0,0 +1,56 @@
+package com.generation.farmaciagen.model;
+
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+	@Entity
+	@Table (name = "tb_categoria")
+	public class Categoria {
+						
+			@Id
+			@GeneratedValue(strategy = GenerationType.IDENTITY)
+			private long id;
+									
+			@NotBlank (message = "Tipo do produto obrigatório!")
+			@Size (min = 5, max = 100, message = "O Tipo do produto deve conter no mínimo 5 caracteres e no máximo 100 caracteres")
+			private String descricao;
+			
+			@OneToMany(mappedBy = "categoria", cascade = CascadeType.REMOVE)
+			@JsonIgnoreProperties("categoria")
+			private List<Produto> produto;
+
+			public long getId() {
+				return id;
+			}
+
+			public void setId(long id) {
+				this.id = id;
+			}
+
+			public String getDescricao() {
+				return descricao;
+			}
+
+			public void setDescricao(String descricao) {
+				this.descricao = descricao;
+			}
+
+			public List<Produto> getProduto() {
+				return produto;
+			}
+
+			public void setProduto(List<Produto> produto) {
+				this.produto = produto;
+			}
+			
+			
+}
---
+++ pharmacyGen/src/main/java/com/generation/farmaciagen/repository/ProdutoRepository.java
@@ -0,0 +1,16 @@
+package com.generation.farmaciagen.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.generation.farmaciagen.model.Produto;
+
+public interface ProdutoRepository  extends JpaRepository <Produto, Long>{
+	
+	public List<Produto> findAllByDescricaoContainingIgnoreCase(@Param("Nome") String nome);
+
+	}
+
+
